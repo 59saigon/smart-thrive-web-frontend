ef40f27a8357376698e008656c33f7ff712e40d7
Son Hoang <nguyenson20zzzz@gmail.com>
2024-05-28 20:27:48 +0700

    init project

--- Smart-Thrive-FE/src/app/app.routes.ts
+++ Smart-Thrive-FE/src/app/app.routes.ts
@@ -1,13 +1,3 @@
 import { Routes } from '@angular/router';
-import { TestComponentComponent } from './test-component/test-component.component';
 
-export const routes: Routes = [
-    {
-        path: 'home',
-        component: TestComponentComponent
-    },
-    {
-        path: '',
-        component: TestComponentComponent
-    }
-];
+export const routes: Routes = [];
