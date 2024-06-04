2bd4aa785054b5282de1f009217744dfb94c79f8
Son Hoang <nguyenson20zzzz@gmail.com>
2024-06-04 14:00:54 +0700

    update: dashboard

--- Smart-Thrive-FE/src/app/main/components/auth/login/login.module.ts
+++ Smart-Thrive-FE/src/app/main/components/auth/login/login.module.ts
@@ -3,7 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
-
+import { ButtonModule } from 'primeng/button';
+import { DividerModule } from 'primeng/divider';
+import { CheckboxModule } from 'primeng/checkbox'
+import { ContextMenuModule } from 'primeng/contextmenu'
 
 @NgModule({
   declarations: [
@@ -11,7 +14,11 @@ import { LoginComponent } from './login.component';
   ],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    ButtonModule,
+    DividerModule,
+    CheckboxModule,
+    LoginRoutingModule,
+    ContextMenuModule,
   ]
 })
 export class LoginModule { }
