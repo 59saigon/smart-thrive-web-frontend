3edd1768790947377de20c0b2c2bf4c57821f522
sonnh37 <nguyenson20zzzz@gmail.com>
2024-06-30 00:02:09 +0700

    light & dark mode

--- Smart-Thrive-FE/src/app/shared/constants/constant-theme.ts
+++ Smart-Thrive-FE/src/app/shared/constants/constant-theme.ts
@@ -3,8 +3,10 @@ const dark = 'lara-dark-blue';
 const darkAtlantis = 'atlantis-dark';
 const darkAura = 'aura-dark-cyan';
 const darkLara = 'lara-dark-cyan';
+const lightLara = 'lara-light-cyan';
 const lightAura = 'aura-light-cyan';
 
 export const ConstantsTheme = {
-  theme: darkLara
+  dark: darkLara,
+  light: lightLara
 } 
