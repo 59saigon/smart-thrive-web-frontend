5bb9e7fa4f43b39c089e8b8854525a5e8b3ae78f
sonnh37 <nguyenson20zzzz@gmail.com>
2024-07-19 14:05:43 +0700

    update

--- Smart-Thrive-FE/src/app/main/services/base/base.service.ts
+++ Smart-Thrive-FE/src/app/main/services/base/base.service.ts
@@ -16,35 +16,55 @@ export class BaseService<TEntity extends BaseEntity> {
   constructor(public http: HttpClient, public _entityTxt: String) {
     this.entityTxt = _entityTxt;
   }
-
+  private getHeaders(): { [header: string]: string | string[] } {
+    return {
+      'ngrok-skip-browser-warning': 'true'
+    };
+  }
   public getBaseUrl(): string {
     return `${ConstantsApi.baseApi}/${this.entityTxt}`;
   }
 
   add(entity: TEntity): Observable<BaseResponse> {
-    return this.http.post<BaseResponse>(`${this.getBaseUrl()}${ConstantsApi.add}`, entity);
+    return this.http.post<BaseResponse>(`${this.getBaseUrl()}${ConstantsApi.add}`, entity, {
+      headers: this.getHeaders()
+    });
   }
-
+  
   update(entity: TEntity): Observable<BaseResponse> {
-    return this.http.put<BaseResponse>(`${this.getBaseUrl()}${ConstantsApi.update}`, entity);
+    return this.http.put<BaseResponse>(`${this.getBaseUrl()}${ConstantsApi.update}`, entity, {
+      headers: this.getHeaders()
+    });
   }
-
+  
   delete(id: Guid): Observable<BaseResponse> {
-    return this.http.put<BaseResponse>(`${this.getBaseUrl()}${ConstantsApi.delete}/?id=${id}`, null);
+    return this.http.put<BaseResponse>(`${this.getBaseUrl()}${ConstantsApi.delete}/?id=${id}`, null, {
+      headers: this.getHeaders()
+    });
   }
-
+  
   getAllPagination(request: PaginatedRequest): Observable<PaginatedListResponse<TEntity>> {
-    return this.http.post<PaginatedListResponse<TEntity>>(`${this.getBaseUrl()}${ConstantsApi.getAllPagination}`, request);
+    return this.http.post<PaginatedListResponse<TEntity>>(`${this.getBaseUrl()}${ConstantsApi.getAllPagination}`, request, {
+      headers: this.getHeaders()
+    });
   }
+  
   getAll(): Observable<ItemListResponse<TEntity>> {
-    return this.http.get<ItemListResponse<TEntity>>(`${this.getBaseUrl()}${ConstantsApi.getAll}`);
+    return this.http.get<ItemListResponse<TEntity>>(`${this.getBaseUrl()}${ConstantsApi.getAll}`, {
+      headers: this.getHeaders()
+    });
   }
-
+  
   getAllSearch(request: PaginatedRequestFillter<TEntity>): Observable<PaginatedListResponse<TEntity>> {
-    return this.http.post<PaginatedListResponse<TEntity>>(`${this.getBaseUrl()}${ConstantsApi.getAllSearch}`, request);
+    return this.http.post<PaginatedListResponse<TEntity>>(`${this.getBaseUrl()}${ConstantsApi.getAllSearch}`, request, {
+      headers: this.getHeaders()
+    });
   }
-
+  
   getById(id: Guid): Observable<ItemResponse<TEntity>> {
-    return this.http.get<ItemResponse<TEntity>>(`${this.getBaseUrl()}${ConstantsApi.getById}/${id}`);
+    return this.http.get<ItemResponse<TEntity>>(`${this.getBaseUrl()}${ConstantsApi.getById}/${id}`, {
+      headers: this.getHeaders()
+    });
   }
+  
 }
